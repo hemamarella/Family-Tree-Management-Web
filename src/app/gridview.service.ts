1114913051eb9630aeeb550989b3edc926aff6c7
Hema <hemamarella723@gmail.com>
2025-01-09 16:30:12 +0530

    gridview and relationview components

--- src/app/gridview.service.ts
+++ src/app/gridview.service.ts
@@ -8,6 +8,7 @@
 //   constructor() { }
 // }
 
+
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -17,7 +18,8 @@ export interface FamilyName {
   Surname: string;
   Color: string;
   Description: string;
-  CreatedAt: Date;
+  CreatedDate: Date;
+  CreatedBy:string
   members: Member[];
 }
 
@@ -27,6 +29,7 @@ export interface Member {
   LastName: string;
   Gender: string;
   DateOfBirth: Date;
+  CreatedDate: Date;
   relationships: Relationship[];
 }
 
@@ -44,11 +47,13 @@ export interface Relationship {
 export class GridviewService {
 
   private apiUrl2='https://localhost:44312/api/FamilyTree/GetFamilyTreeData1';
-  private apiUrl3='https://localhost:44312/api/FamilyTree/DeleteFamily';
+  private apiUrl3='https://localhost:44312/api/FamilyTree/api/family/delete';
   private apiUrl4='https://localhost:44312/api/FamilyTree/DeleteRelationship';
   private apiUrl5=' https://localhost:44312/api/FamilyTree/api/FamilyNames';
-  private apiUrl6='https://localhost:44312/api/FamilyTree/api/Members'
+  private apiUrl6='https://localhost:44312/api/FamilyTree/api/member/delete'
   private apiUrl7='https://localhost:44312/api/FamilyTree/api/MemberRelations'
+  private apiUrl10='https://localhost:44312/api/FamilyTree/api/Members'
+  private apiUrl11='https://localhost:44312/api/FamilyTree/api/Relations'
 
 
 
@@ -57,24 +62,37 @@ export class GridviewService {
   getFamilyNames1(): Observable<any> {
     return this.http.get(this.apiUrl2);
   }
+  getRelation(): Observable<any> {
+    return this.http.get(this.apiUrl11);
+  }
 
   updateFamilyName(id: number, familyName: any): Observable<any> {
     return this.http.put(`${this.apiUrl5}/${id}`, familyName);
   }
   UpdateMembers(id: number, member: any): Observable<any> {
-    return this.http.put(`${this.apiUrl6}/${id}`, member);
+    return this.http.put(`${this.apiUrl10}/${id}`, member);
   }
   UpdateMemberRelations(id: number, membersRelation: any): Observable<any> {
     return this.http.put(`${this.apiUrl7}/${id}`, membersRelation);
   }
 
-deleteFamily(id: number): Observable<any> {
-  return this.http.delete(`${this.apiUrl3}/${id}`);
-}
+// deleteFamily(id: number): Observable<any> {
+//   return this.http.delete(`${this.apiUrl3}/${id}`);
+// }
 deleteRelation(id: number): Observable<any> {
   return this.http.delete(`${this.apiUrl4}/${id}`);
 }
 
+deleteFamily(id: number): Observable<any> {
+  return this.http.delete(`${this.apiUrl3}/${id}`);
+}
+
+deleteMember(memberId: number): Observable<any> {
+  return this.http.delete(`${this.apiUrl6}/${memberId}`);
+}
+
+
 
 
 }
+
